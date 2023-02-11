90e2adb562485e2635cfb38cb728ae775e0c79a6
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-02-11 20:59:07 +0100

    Menu and option.

--- Dialog/app/src/main/java/com/training/codeinlove/myapplication/MainActivity.kt
+++ Dialog/app/src/main/java/com/training/codeinlove/myapplication/MainActivity.kt
@@ -3,7 +3,10 @@ package com.training.codeinlove.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +28,22 @@ class MainActivity : AppCompatActivity() {
             }
             fragment.show(supportFragmentManager, "confirmDialog")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
+        R.id.acton_add -> {
+            Toast.makeText(this, "Ajouter", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.acton_delete -> {
+            Toast.makeText(this, "Supprimer", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
