082eb8e75d2b4db70aa4f4464d77f99a3d132efd
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-02-14 08:20:57 +0100

    Add new note button

--- notes/app/src/main/java/com/training/codeinlove/notes/MainActivity.kt
+++ notes/app/src/main/java/com/training/codeinlove/notes/MainActivity.kt
@@ -3,12 +3,13 @@ package com.training.codeinlove.notes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.preference.PreferenceManager.OnActivityResultListener
-import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+private const val NEW_NOTE_INDEX = -1
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        findViewById<FloatingActionButton>(R.id.create_note_fab).setOnClickListener(this)
+
         notes = mutableListOf()
         notes.add(Note("Note 1", "Blablabla"))
         notes.add(Note("Memo Bob", "Range ton linge"))
@@ -33,7 +36,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(view: View) {
         if (view.tag != null) {
             showNoteDetail(view.tag as Int)
+        } else {
+            when(view.id) {
+                R.id.create_note_fab -> createNewNote()
+            }
         }
+    }
+
+    private fun createNewNote() {
+        showNoteDetail(NEW_NOTE_INDEX)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -47,18 +58,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun processEditNoteResult(data: Intent) {
-        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, -1)
+        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, NEW_NOTE_INDEX)
         val note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRA_NOTE)
         saveNote(note!!, noteIndex)
     }
 
     private fun saveNote(note: Note, noteIndex: Int) {
-        notes[noteIndex] = note
+        if (noteIndex < 0) {
+            notes.add(0, note)
+        } else {
+            notes[noteIndex] = note
+        }
         adapter.notifyDataSetChanged()
     }
 
     private fun showNoteDetail(noteIndex: Int) {
-        val note = notes[noteIndex]
+        val note = if (noteIndex < 0) Note() else notes[noteIndex]
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, noteIndex)
