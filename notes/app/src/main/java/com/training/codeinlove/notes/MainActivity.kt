fb496369ab5739b73bc5758d16667e42618bed22
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-02-14 07:59:32 +0100

    Note Project
    Bouton save.

--- notes/app/src/main/java/com/training/codeinlove/notes/MainActivity.kt
+++ notes/app/src/main/java/com/training/codeinlove/notes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.training.codeinlove.notes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager.OnActivityResultListener
 import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.Toolbar
@@ -27,7 +28,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val recycleView = findViewById<RecyclerView>(R.id.notes_recycle_view)
         recycleView.layoutManager = LinearLayoutManager(this)
         recycleView.adapter = adapter
-
     }
 
     override fun onClick(view: View) {
@@ -36,11 +36,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != RESULT_OK || data == null) {
+            return
+        }
+        when (requestCode) {
+            NoteDetailActivity.REQUEST_EDIT_NOTE -> processEditNoteResult(data)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun processEditNoteResult(data: Intent) {
+        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, -1)
+        val note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRA_NOTE)
+        saveNote(note!!, noteIndex)
+    }
+
+    private fun saveNote(note: Note, noteIndex: Int) {
+        notes[noteIndex] = note
+        adapter.notifyDataSetChanged()
+    }
+
     private fun showNoteDetail(noteIndex: Int) {
         val note = notes[noteIndex]
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, noteIndex)
-        startActivity(intent)
+        startActivityForResult(intent, NoteDetailActivity.REQUEST_EDIT_NOTE)
     }
 }
--- notes/app/src/main/java/com/training/codeinlove/notes/NoteDetailActivity.kt
+++ notes/app/src/main/java/com/training/codeinlove/notes/NoteDetailActivity.kt
@@ -1,12 +1,16 @@
 package com.training.codeinlove.notes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
 class NoteDetailActivity : AppCompatActivity() {
     companion object {
+        val REQUEST_EDIT_NOTE = 1
         val EXTRA_NOTE = "note"
         val EXTRA_NOTE_INDEX = "noteIndex"
     }
@@ -30,5 +34,28 @@ class NoteDetailActivity : AppCompatActivity() {
 
         titleView.text = note.title
         textView.text = note.title
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_note_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_save -> saveNote()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun saveNote(): Boolean {
+        note.title = titleView.text.toString()
+        note.text = textView.text.toString()
+        val intent = Intent()
+        intent.putExtra(EXTRA_NOTE, note)
+        intent.putExtra(EXTRA_NOTE_INDEX, nodeIndex)
+        setResult(RESULT_OK, intent)
+        finish()
+        return true
     }
 }
